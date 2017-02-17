369b6dac5155a590ee39cb433f5b9af3a7bdf4a6
cthulhuhoop <akj@leng.local>
2017-02-17 14:12:40 +0000

    style active links

--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'nav-bar',
-    templateUrl: 'navbar.component.html'
+    templateUrl: 'navbar.component.html',
+    styles: [`
+        li > a.active {color: orange;}
+    `]
 })
 export class NavBarComponent implements OnInit {
     constructor() { }
